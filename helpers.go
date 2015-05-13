307be42ab112f3311f6aca285293c703a5f4e0d6
Jacob Santos <github@jacobsantos.com>
2015-05-13 11:19:32 -0500

    Move byte and string helpers to new file. Going to move creating vectors to vector.go, which will create any arbitrary supported SafeArray

---
+++ helpers.go
@@ -0,0 +1,76 @@
+package safearray
+
+import (
+	"errors"
+	"reflect"
+	"unsafe"
+
+	"github.com/go-ole/com"
+)
+
+// IsSlice checks whether interface{} type is a slice.
+func IsSlice(value interface{}) bool {
+	return reflect.ValueOf(value).Kind() != reflect.Slice
+}
+
+// GetElementString retrieves string from index.
+func GetElementString(safearray *COMArray, index int64) (str string, err error) {
+	var element *int16
+	err = PutElementIn(safearray, index, &element)
+	str = com.BstrToString(*(**uint16)(unsafe.Pointer(&element)))
+	com.SysFreeString(element)
+	return
+}
+
+// FromByteArray creates SafeArray from byte array.
+func FromByteArray(slice []byte) (safearray *COMArray, err error) {
+	safearray, err = CreateArrayVector(com.UInteger8VariantType, 0, uint32(len(slice)))
+
+	if safearray == nil {
+		err = errors.New("could not convert []byte to SafeArray")
+		return
+	}
+
+	for i, v := range slice {
+		PutElement(safearray, int64(i), uintptr(unsafe.Pointer(&v)))
+	}
+	return safearray
+}
+
+// ToByteArray converts SafeArray to byte array.
+//
+// This will convert a multidimensional SafeArray to a single dimensional Go
+// slice.
+func ToByteArray(safearray *Array) (bytes []byte, err error) {
+	bytes = make([]byte, safearray.Length())
+	err = safearray.PutInArray(&bytes)
+	return
+}
+
+// ToStringArray converts SafeArray object to string array.
+//
+// This will convert a multidimensional SafeArray to a single dimensional Go
+// slice.
+func ToStringArray(safearray *Array) (strings []string) {
+	strings = make([]string, safearray.Length())
+	err = safearray.PutInArray(&strings)
+	return
+}
+
+// MarshalArray accesses SafeArray data to quickly convert to Go array.
+func MarshalArray(safearray *COMArray, length int64, slice interface{}) (err error) {
+	// Single dimensional arrays are faster, if you use AccessData() and
+	// UnaccessData().
+	ptr, err := AccessData(safearray)
+	if err != nil {
+		return
+	}
+
+	err = com.PointerToArrayAppend(ptr, length, &slice)
+	if err != nil {
+		return
+	}
+
+	err = UnaccessData(safearray)
+	return
+}
